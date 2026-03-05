fd72abebf87f32b72a63b2514d477799340e9363
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2026-03-06 05:08:47 +0700

    menggagalkan test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,31 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestHelloWorld(t *testing.T) {
+func TestHelloWorldSyafiq(t *testing.T) {
 	result := HelloWorld("Syafiq")
 	if result != "Hello Syafiq" {
 		// error
-		panic("Result is not 'Hello Syafiq'")
+		// t.Fail()
+		t.Error("Result must be 'Hello Syafiq'")
 	}
+
+	fmt.Println("TestHelloWorldSyafiq Done")
 }
+
+func TestHelloWorldEmha(t *testing.T) {
+	result := HelloWorld("Emha")
+	if result != "Hello Emha" {
+		// error
+		// t.FailNow()
+		t.Error("Result must be 'Hello Emha'")
+	}
+
+	fmt.Println("TestHelloWorldEmha Done")
+}
+
+// go test -v menjalakan semua unit test
+// go test -v -run=TestHelloWorldSyafiq menjalankan spesifik test
